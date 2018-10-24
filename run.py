baf2af67399ab7017094bbb034c38989f116ddfc
James Wafula <wafulajames9@gmail.com>
2018-10-24 23:09:56 +0300

    [finishes #161461941] Checking deployment blocker

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app
 
-config_name = os.getenv('FLASK_ENV')
+config_name = os.getenv('FLASK_ENV', 'development')
 app = create_app(config_name)
 
 if __name__ == '__main__':
