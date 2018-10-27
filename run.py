78a0fd15d1ad844af8d3b57aee5a0892c49687f0
james <42789760+jamesbeamie@users.noreply.github.com>
2018-10-28 02:40:42 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app
 
-config_name = os.getenv('FLASK_ENV','development')
+config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
 
 if __name__ == '__main__':
